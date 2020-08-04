934c3150a41379807e862dbb2fc0c5fdca235033
Giselle Chacon <gisecn@gmail.com>
2020-08-04 10:37:27 +0000

    Addeed stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51HC8P1Heew9pZh4QWYPg5l1n5w0nIhn5VDBJXZdFmdY0WOKKC1A6HOOCx7Atxl8EvVT8jjUGbPACB70i184RIXc700jmxSpNMl',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
